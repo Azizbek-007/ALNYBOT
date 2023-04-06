d64a578418e1e3985860618f16fceaa13176cc83
Azizbek Berdimuratov <77209269+Azizbek-007@users.noreply.github.com>
2023-04-07 01:13:07 +0500

    DSsdxdsd

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -54,7 +54,8 @@ async def RLGetAlllm(msg: types.Message, state: FSMContext):
         print(data)
         await msg.reply("✅", reply_markup=back_btn)
         for x in data:
-            await dp.bot.copy_message(msg.from_id, x[2], x[1], reply_markup=delete_btn(x[0]))
+            print(x)
+            # await dp.bot.copy_message(msg.from_id, x[2], x[1], reply_markup=delete_btn(x[0]))
     else:
         await msg.reply("Not Found", reply_markup=admin_btn())
 
