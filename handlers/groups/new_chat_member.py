b0af63d4335abf5ad53f8a26231777fe95bb76e7
Azizbek Berdimuratov <77209269+Azizbek-007@users.noreply.github.com>
2023-04-08 21:38:01 +0500

    Update new_chat_member.py

--- handlers/groups/new_chat_member.py
+++ handlers/groups/new_chat_member.py
@@ -8,6 +8,7 @@ from utils.db_api import DBS
 @dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def new_chat_member_bot(msg: types.Message):
     new_members = msg.new_chat_members
+    print(new_members)
     for x in new_members:
         if x.id != msg.from_id:
-            DBS.add_count(DBS, msg.from_id, msg.chat.id)            
+            DBS.add_count(DBS, msg.from_id, msg.chat.id)
